92a8f3d0104cbfce9eb7e9b8679c5c8f5b92d0a3
ckrountree <jmail0207@gmail.com>
2018-02-03 16:14:48 -0800

    get audio piece working

--- src/components/controls/AudioPlayer.js
+++ src/components/controls/AudioPlayer.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 export default class AudioPlayer extends Component {
     render() {
+      console.log(this.props.url);
       return (
-        <audio 
-          src={this.props.audio}
-          ref={(audio) => { this.audio = audio }}
-          autoPlay>
+        <audio src={this.props.url}>
             Your browser does not support the <code>audio</code> element.
         </audio>
       ) 
     }
 }
+
+ 
