96d1cd27702f462443f62e1ae02431c30c1807bd
ckrountree <jmail0207@gmail.com>
2018-02-04 20:15:32 -0800

    remove unused imports and delete uncommented code

--- src/components/controls/Speak.js
+++ src/components/controls/Speak.js
@@ -1,51 +1,17 @@
 import React, { Component } from 'react';
-import ReactAudioPlayer from 'react-audio-player';
-import AudioPlayer from './AudioPlayer';
 import PropTypes from 'prop-types';
 
 export default class Speak extends Component {
   constructor(props) {
     super(props);
     this.audio = null;
-    // this.handleClick = this.handleClick.bind(this);
   }
-
-  // componentDidMount() {
-  //   const audio = this.audio
-  //   // enough downloaded to begin play
-  //   audio.addEventListener((e) => {
-  //     this.props.onReadyPlay && this.props.onReadyPlay(e);
-  //   });
-  // }
 
   onPlay = () => {
     this.audio.play()
-    // if (this.state.audioOn) {
-    //   this.setState({audioOn: false});
-    //   this.audioOn.onPause();
-    // } else {
-    //   this.setState({ audioOn: true });
-    //   this.audioOn.onPlay();
-    // }
   }
 
   render() {
-    // return (
-    //   <div>
-    //     <AudioPlayer/>
-    //   <div 
-    //     onClick={this.onPlay} 
-    //     className={!this.state.audioOn ? 'Play' : 'Pause'}>
-    //     <ReactAudioPlayer
-    //       src={this.state.url}
-    //       ref={(audio) => { this.audioOn = audio; }}
-    //       autoPlay
-    //       preload= "auto"
-    //       volume={0.5}
-    //     />
-    //   </div>
-    //   </div>
-    // );
     return (
       <div>
         <audio 
